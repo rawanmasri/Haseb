cb0009c3a6d1607f0cc2767a7cdbd78f3fa1c9b1
rawanmasri <rawan.m.masri@gmail.com>
2014-08-07 12:46:12 +0300

    init
    test

--- src/com/asaltech/haseb/activities/CreatGroup.java
+++ src/com/asaltech/haseb/activities/CreatGroup.java
@@ -37,6 +37,10 @@ public class CreatGroup extends ActionBarActivity {
 	private SimpleAdapter simpleAdpt ;
 	private ListView lView;
     private EditText groupName;
+    
+    
+    
+    
     private  EditText groupCurrency;
     
     private EditText groupMember;
