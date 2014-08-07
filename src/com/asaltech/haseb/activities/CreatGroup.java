757acaec7dd016723ca4ed71f2716e6c68d989d7
rawanmasri <rawan.m.masri@gmail.com>
2014-08-07 12:40:22 +0300

    change line
    test commit

--- src/com/asaltech/haseb/activities/CreatGroup.java
+++ src/com/asaltech/haseb/activities/CreatGroup.java
@@ -37,6 +37,7 @@ public class CreatGroup extends ActionBarActivity {
 	private SimpleAdapter simpleAdpt ;
 	private ListView lView;
     private EditText groupName;
+    
     private  EditText groupCurrency;
     
     private EditText groupMember;
