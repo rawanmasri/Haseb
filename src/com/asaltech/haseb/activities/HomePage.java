bee2d5b256381baa11de15ad401c11316b0b3267
rawanmasri <rawan.m.masri@gmail.com>
2014-08-11 10:34:21 +0300

    code review for the home page

--- src/com/asaltech/haseb/activities/HomePage.java
+++ src/com/asaltech/haseb/activities/HomePage.java
@@ -1,41 +1,19 @@
 package com.asaltech.haseb.activities;
 
 import java.util.Locale;
-
 import android.app.TabActivity;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.widget.TabHost;
-import android.widget.Toast;
 import android.widget.TabHost.TabSpec;
-
-import com.asaltech.haseb.utils.SharedPref;
-import com.example.bean.User;
-
+@SuppressWarnings("deprecation")
 public class HomePage extends TabActivity {
 	// TabSpec Names
 	Bundle userData;
-	
 	public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
-        
-        
         TabHost tabHost = getTabHost();
-        
-        // Home tab
-       
-         
-        // Tab for Setting
-        
-         
-        // Tab for notification
-        TabSpec notificationspec = tabHost.newTabSpec("Notification");
-        notificationspec.setIndicator("Notification", getResources().getDrawable(R.drawable.notificationxml));
-        Intent notificationIntent = new Intent(this, NotificationActivity.class);
-        notificationspec.setContent(notificationIntent);
-         
         if(Locale.getDefault().getLanguage().equals("en")) {
         	 TabSpec homespec = tabHost.newTabSpec("Home");
              // setting Title and Icon for the Tab
@@ -46,8 +24,8 @@ public class HomePage extends TabActivity {
              settingspec.setIndicator("Setting", getResources().getDrawable(R.drawable.settingxml));
              Intent settingIntent = new Intent(this, MyAccountActivity.class);
              settingspec.setContent(settingIntent);
-        	 tabHost.addTab(homespec); // Adding photos tab
-             tabHost.addTab(settingspec); // Adding songs tab
+        	 tabHost.addTab(homespec); 
+             tabHost.addTab(settingspec);
         } else {
         	 TabSpec homespec = tabHost.newTabSpec("«·—∆Ì”Ì…");
              // setting Title and Icon for the Tab
@@ -58,19 +36,8 @@ public class HomePage extends TabActivity {
              settingspec.setIndicator("«·«⁄œ«œ« ", getResources().getDrawable(R.drawable.settingxml));
              Intent settingIntent = new Intent(this, MyAccountActivity.class);
              settingspec.setContent(settingIntent);
-        	 tabHost.addTab(settingspec); // Adding songs tab
-        	 tabHost.addTab(homespec); // Adding photos tab
-        } 
-        	
-        // Adding all TabSpec to TabHost
-       
-     //   tabHost.addTab(notificationspec); // Adding videos tab
-         
-       
+        	 tabHost.addTab(settingspec); 
+        	 tabHost.addTab(homespec);
+        }        
     }
-	
-	
-	
-	
-	
 }
